e584813342c84d6511353a1de0794d60bcd48434
KiraButter <70840158+KiraButter@users.noreply.github.com>
2020-09-29 11:16:24 +0300

    //

--- LR-2/script.js
+++ LR-2/script.js
@@ -4,20 +4,24 @@ function quad(a,b,c,d,e,f) {
 	var y2 = (f - d*x)/e;
 	var y;
 	
-	if ((d*b - a*e) == 0) {
+	if (b == 0 && e == 0) {
 		return "Решения системы нет";
 	}
-	//else if (b == 0) {
-		//y = y2;
-		//return "X = " + x + "\nY = " + y;
-	//}
-	//else if (e == 0) {
-		//y = y1;
-		//return "X = " + x + "\nY = " + y;
-	//}
-	//else if (b == - && e == 0) {
-		//return "Решения системы нет";
-	//}
+	else if ((d*b - a*e) == 0) {
+		return "Решения системы нет";
+	}
+	else if (b == 0) {
+		y = y2;
+		return "X = " + x.toFixed(2) + "\nY = " + y.toFixed(2);
+	}
+	else if (e == 0) {
+		y = y1;
+		return "X = " + x.toFixed(2) + "\nY = " + y.toFixed(2);
+	}
+	else  {
+		y = y2;
+		return "X = " + x.toFixed(2) + "\nY = " + y.toFixed(2);
+	}
 }
 
 function prog() {
